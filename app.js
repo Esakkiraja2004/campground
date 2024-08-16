74f3c8d7cb22b27654328cc53dc0e125582d7044
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-08-16 19:25:07 +0530

    FM

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const reviewSchema = require('./models/review');
 const methodOverride = require('method-override')
 const app = express();
 const port = 3000;
+const campgroundRoute = require('./routes/campground');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -40,96 +41,8 @@ app.set('views' , path.join(__dirname, 'view'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const joiSchema = joi.object({
-  title : joi.string().required(),
-  location : joi.string().required(),
-  place: joi.string().required(),
-  description:joi.string().required(),
-  image: joi.string().required(),
-  price: joi.number().required()
-})
+app.use('camgrrounds',campgroundRoute);
 
-app.get('/',(req, res) =>{
-    res.render("index.ejs");
-});
-
-app.get('/campgrounds' ,catchError( async(req, res) =>{
-    const camp = await cammod.find({});
-    res.render('campground/camp' , { camp});
-}));
-
-app.get('/campgrounds/newcamp',(req, res) => {
-
-  res.render('campground/new.ejs');
-
-})
-
-app.get('/campgrounds/:id' , catchError(async(req, res) =>{
-    const {id} = req.params;
-    const camp = await cammod.findById(id).populate('reviews');
-    res.render('campground/show.ejs' , { camp});
-}));
-
-app.post('/campgrounds/:id/review' , catchError(async(req,res) =>{
-  
-  const {id} = req.params;
-  const camp = await cammod.findById(id);
-
-  const {review , rating} =  req.body;
-  const reviews_data = await reviewSchema({body : review, rating});
-
-  await reviews_data.save();
-  camp.reviews.push(reviews_data._id);
-  await camp.save();
-
-  res.redirect(`/campgrounds/${id}`)
-
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchError(async (req, res) => {
-  const { id, reviewId } = req.params;
-
-  // Pull the reviewId from the camp's reviews array
-  await cammod.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-
-  // Delete the review document itself
-  await reviewSchema.findByIdAndDelete(reviewId);
-
-  res.redirect(`/campgrounds/${id}`);
-}));
-
-
-app.post('/campgrounds', catchError(async (req, res) => {
-  const { error } = joiSchema.validate(req.body);
-  if (error) return res.status(400).render('campground/error', { err: error });
-  const data = await new cammod(req.body);
-  await data.save();
-  res.redirect('/campgrounds');
-
-}));
-
-app.get('/campgrounds/:id/edit', catchError(async (req, res) => {
-  const {id} = req.params;
-  const camp = await cammod.findById(id);
-  res.render('campground/edit.ejs', {camp});
-}));
-
-app.put('/campgrounds/:id', catchError(async (req, res) => {
-  const { error } = joiSchema.validate(req.body);
-  if (error) return res.status(400).render('campground/error', { err: error });
-  const {id} = req.params;
-  const camp = await cammod.findByIdAndUpdate(id,{...req.body});
-  await camp.save();
-  res.redirect(`/campgrounds/${id}`);
-}));
-
-app.delete('/campgrounds/:id', catchError(async (req, res) => {
-
-  const {id} = req.params;
-  await cammod.findByIdAndDelete(id);
-  res.redirect('/campgrounds');
-
-}));
 
 app.all("*", (req, res, next) => {
 
