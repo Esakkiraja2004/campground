47f163129ea46b2b591921c2a3d51b8f2c689eb9
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-08-16 20:22:25 +0530

    FM

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-const express  = require('express');
+const express = require('express');
 const ejsMate = require('ejs-mate');
-const catchError = require('./utils/catchError');
-const expressError = require('./utils/expresserror')
-const joi  = require('joi');
+const expressError = require('./utils/expresserror');
+const methodOverride = require('method-override');
 const path = require('path');
-const cammod = require('./models/schema');
-const reviewSchema = require('./models/review');
-const methodOverride = require('method-override')
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
-const campgroundRoute = require('./routes/campground');
+
+const campgroundsRoutes = require('./routes/campground');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
-// Import mongoose module
-const mongoose = require('mongoose');
-
-// Define your MongoDB URI
 const mongoURI = 'mongodb://localhost:27017/campground';
 
-// Function to connect to the database
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true,});
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Failed to connect to MongoDB', err);
@@ -32,30 +25,25 @@ const connectDB = async () => {
   }
 };
 
-// Call the connectDB function
 connectDB();
 
-app.engine('ejs' , ejsMate)
-app.set('view engine','ejs');
-app.set('views' , path.join(__dirname, 'view'));
-
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('camgrrounds',campgroundRoute);
-
+app.use('/campgrounds', campgroundsRoutes);
 
 app.all("*", (req, res, next) => {
-
-  next(new expressError('OOPS page not found!!',404));
+  next(new expressError('OOPS page not found!!', 404));
 });
 
-app.use((err, req, res, next) =>{
-  const{status = 500} = err;
-  if(!err.message) err.message ="oops somthing went wrong!!!"
-  res.status(status).render('campground/error', {err});
+app.use((err, req, res, next) => {
+  const { status = 500 } = err;
+  if (!err.message) err.message = "oops somthing went wrong!!!";
+  res.status(status).render('campground/error', { err });
 });
 
-app.listen(port , () =>{
-    console.log('listening on port 3000');
-
+app.listen(port, () => {
+  console.log('listening on port 3000');
 });
--- routes/campground.js
+++ routes/campground.js
@@ -1,105 +1,79 @@
-const express  = require('express');
-const ejsMate = require('ejs-mate');
+const express = require('express');
+const router = express.Router();
 const catchError = require('../utils/catchError');
-const expressError = require('../utils/expresserror')
-const joi  = require('joi');
-const path = require('path');
 const cammod = require('../models/schema');
 const reviewSchema = require('../models/review');
-const methodOverride = require('method-override')
-const app = express();
-const router = express.Router();
-
-
-
+const joi = require('joi');
 
 const joiSchema = joi.object({
-    title : joi.string().required(),
-    location : joi.string().required(),
-    place: joi.string().required(),
-    description:joi.string().required(),
-    image: joi.string().required(),
-    price: joi.number().required()
-  })
-  
-  router.get('/',(req, res) =>{
-      res.render("index.ejs");
-  });
-  
-  router.get('/' ,catchError( async(req, res) =>{
-      const camp = await cammod.find({});
-      res.render('campground/camp' , { camp});
-  }));
-  
-  router.get('/newcamp',(req, res) => {
-  
-    res.render('campground/new.ejs');
-  
-  })
-  
-  router.get('/:id' , catchError(async(req, res) =>{
-      const {id} = req.params;
-      const camp = await cammod.findById(id).populate('reviews');
-      res.render('campground/show.ejs' , { camp});
-  }));
-  
-  router.post('/:id/review' , catchError(async(req,res) =>{
-    
-    const {id} = req.params;
-    const camp = await cammod.findById(id);
-  
-    const {review , rating} =  req.body;
-    const reviews_data = await reviewSchema({body : review, rating});
-  
-    await reviews_data.save();
-    camp.reviews.push(reviews_data._id);
-    await camp.save();
-  
-    res.redirect(`/${id}`)
-  
-  }))
-  
-  router.delete('/:id/reviews/:reviewId', catchError(async (req, res) => {
-    const { id, reviewId } = req.params;
-  
-    // Pull the reviewId from the camp's reviews array
-    await cammod.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  
-    // Delete the review document itself
-    await reviewSchema.findByIdAndDelete(reviewId);
-  
-    res.redirect(`//${id}`);
-  }));
-  
-  
-  router.post('/', catchError(async (req, res) => {
-    const { error } = joiSchema.validate(req.body);
-    if (error) return res.status(400).render('campground/error', { err: error });
-    const data = await new cammod(req.body);
-    await data.save();
-    res.redirect('/');
-  
-  }));
-  
-  router.get('/:id/edit', catchError(async (req, res) => {
-    const {id} = req.params;
-    const camp = await cammod.findById(id);
-    res.render('campground/edit.ejs', {camp});
-  }));
-  
-  router.put('/:id', catchError(async (req, res) => {
-    const { error } = joiSchema.validate(req.body);
-    if (error) return res.status(400).render('campground/error', { err: error });
-    const {id} = req.params;
-    const camp = await cammod.findByIdAndUpdate(id,{...req.body});
-    await camp.save();
-    res.redirect(`//${id}`);
-  }));
-  
-  router.delete('/:id', catchError(async (req, res) => {
-  
-    const {id} = req.params;
-    await cammod.findByIdAndDelete(id);
-    res.redirect('/');
-  
-  }));
+  title: joi.string().required(),
+  location: joi.string().required(),
+  place: joi.string().required(),
+  description: joi.string().required(),
+  image: joi.string().required(),
+  price: joi.number().required()
+});
+
+router.get('/', catchError(async (req, res) => {
+  const camp = await cammod.find({});
+  res.render('campground/camp', { camp });
+}));
+
+router.get('/newcamp', (req, res) => {
+  res.render('campground/new.ejs');
+});
+
+router.get('/:id', catchError(async (req, res) => {
+  const { id } = req.params;
+  const camp = await cammod.findById(id).populate('reviews');
+  res.render('campground/show.ejs', { camp });
+}));
+
+router.post('/:id/review', catchError(async (req, res) => {
+  const { id } = req.params;
+  const camp = await cammod.findById(id);
+  const { review, rating } = req.body;
+  const reviews_data = await reviewSchema({ body: review, rating });
+  await reviews_data.save();
+  camp.reviews.push(reviews_data._id);
+  await camp.save();
+  res.redirect(`/campgrounds/${id}`);
+}));
+
+router.delete('/:id/reviews/:reviewId', catchError(async (req, res) => {
+  const { id, reviewId } = req.params;
+  await cammod.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await reviewSchema.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
+}));
+
+router.post('/', catchError(async (req, res) => {
+  const { error } = joiSchema.validate(req.body);
+  if (error) return res.status(400).render('campground/error', { err: error });
+  const data = await new cammod(req.body);
+  await data.save();
+  res.redirect('/campgrounds');
+}));
+
+router.get('/:id/edit', catchError(async (req, res) => {
+  const { id } = req.params;
+  const camp = await cammod.findById(id);
+  res.render('campground/edit.ejs', { camp });
+}));
+
+router.put('/:id', catchError(async (req, res) => {
+  const { error } = joiSchema.validate(req.body);
+  if (error) return res.status(400).render('campground/error', { err: error });
+  const { id } = req.params;
+  const camp = await cammod.findByIdAndUpdate(id, { ...req.body });
+  await camp.save();
+  res.redirect(`/campgrounds/${id}`);
+}));
+
+router.delete('/:id', catchError(async (req, res) => {
+  const { id } = req.params;
+  await cammod.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
+}));
+
+module.exports = router;
